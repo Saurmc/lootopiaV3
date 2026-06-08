0936ff119c29f14573d6f5078101452737445132
Paul-Antoine CARREAU <pcarreau@ascent-formation.fr>
2026-06-08 22:40:59 +0200

    chore(mobile): update eas.json and api url for android device build

--- apps/mobile/src/constants/api.constants.ts
+++ apps/mobile/src/constants/api.constants.ts
@@ -4,5 +4,5 @@ import { Platform } from 'react-native';
 // Sur iOS (simulateur ou vrai appareil), localhost fonctionne directement
 export const API_BASE_URL =
   Platform.OS === 'android'
-    ? 'http://10.0.2.2:3000'
-    : 'http://192.168.1.14:3000';
+    ? 'http://192.168.1.162:3000'
+    : 'http://192.168.1.162:3000';
